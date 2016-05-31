786e0ae4cb12b00928e767450f2ae8cbe4c41f35
Rowland Rose <rowlandrose@gmail.com>
2016-05-30 23:50:20 -0400

    Reverting

--- dist/app/redditApp.component.js
+++ dist/app/redditApp.component.js
@@ -11,20 +11,13 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var article_component_1 = require('./article/article.component');
 var article_data_service_1 = require('./article-data.service');
-var articles_data_service_1 = require('./articles-data.service');
 var redditApp = (function () {
     function redditApp() {
-        /*this.articles = [
-            new ArticleDataService('Angular 2', 'http://angular.io', 3),
-            new ArticleDataService('Fullstack', 'http://fullstack.io', 2),
-            new ArticleDataService('Angular Homepage', 'http://angular.io', 1),
-        ];*/
-        this.articles_data = new articles_data_service_1.ArticlesDataService(this.http);
-        var articles_json = this.articles_data.getArticles();
-        for (var i = 0; i < articles_json.data.articles.length; i++) {
-            var a = articles_json.data.articles[i];
-            this.articles.push(new article_data_service_1.ArticleDataService(a.title, a.link, a.votes));
-        }
+        this.articles = [
+            new article_data_service_1.ArticleDataService('Angular 2', 'http://angular.io', 3),
+            new article_data_service_1.ArticleDataService('Fullstack', 'http://fullstack.io', 2),
+            new article_data_service_1.ArticleDataService('Angular Homepage', 'http://angular.io', 1),
+        ];
     }
     redditApp.prototype.addArticle = function (title, link) {
         console.log("Adding article title: " + title.value + " and link: " + link.value);
@@ -38,8 +31,7 @@ var redditApp = (function () {
             selector: 'reddit',
             templateUrl: 'redditApp.component.html',
             styleUrls: ['redditApp.component.css'],
-            directives: [article_component_1.ArticleComponent],
-            providers: [articles_data_service_1.ArticlesDataService]
+            directives: [article_component_1.ArticleComponent]
         }), 
         __metadata('design:paramtypes', [])
     ], redditApp);
--- src/app/redditApp.component.ts
+++ src/app/redditApp.component.ts
@@ -1,37 +1,24 @@
 import { Component } from '@angular/core';
 import { ArticleComponent } from './article/article.component';
-import {Http, Response} from '@angular/http';
 import {ArticleDataService} from './article-data.service';
-import {ArticlesDataService} from './articles-data.service';
 
 @Component({
   moduleId: module.id,
   selector: 'reddit',
   templateUrl: 'redditApp.component.html',
   styleUrls: ['redditApp.component.css'],
-  directives: [ArticleComponent],
-  providers: [ArticlesDataService] 
+  directives: [ArticleComponent] 
 })
 export class redditApp {
 	
 	articles: ArticleDataService[];
-	articles_data: ArticlesDataService;
-	http:Http;
 
 	constructor() {
-		/*this.articles = [
+		this.articles = [
 			new ArticleDataService('Angular 2', 'http://angular.io', 3),
 			new ArticleDataService('Fullstack', 'http://fullstack.io', 2),
 			new ArticleDataService('Angular Homepage', 'http://angular.io', 1),
-		];*/
-		this.articles_data = new ArticlesDataService(this.http);
-		let articles_json = this.articles_data.getArticles();
-		for(let i = 0; i < articles_json.data.articles.length; i++) {
-			let a = articles_json.data.articles[i];
-			this.articles.push(
-				new ArticleDataService(a.title, a.link, a.votes)
-			);
-		}
+		];
 	}
 
 	addArticle(title: HTMLInputElement, link: HTMLInputElement): void {
