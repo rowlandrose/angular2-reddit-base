245009bd5a4bdd6e14fabf19d3a2b01f4c533ae4
Rowland Rose <rowlandrose@gmail.com>
2016-05-30 23:38:45 -0400

    data fix attempt

--- src/app/articles-data.service.ts
+++ src/app/articles-data.service.ts
@@ -9,30 +9,24 @@ export class ArticlesDataService {
  
   // Uses http.get() to load a single JSON file
   getArticles() {
+  	let json = {'data':{'articles':[]}};
+  	json.data.articles.push({
+  		'title':'Angular 2',
+  		'link':'http://angular.io',
+  		'votes':3
+  	});
+  	json.data.articles.push({
+  		'title':'Fullstack',
+  		'link':'http://fullstack.io',
+  		'votes':16
+  	});
+  	json.data.articles.push({
+  		'title':'Rowland\'s Homepage',
+  		'link':'http://rowlandrose.com',
+  		'votes':57
+  	});
     //return this.http.get('https://dl.dropboxusercontent.com/s/8ijycefu7mabr9g/sample_data.json?dl=0').map((res:Response) => res.json());
-    return JSON.parse(`
-    	[{
-			"data": {
-				"articles": [
-					{
-						"title":"Angular 2",
-						"link":"http://angular.io",
-						"votes":3
-					},
-					{
-						"title":"Fullstack",
-						"link":"ttp://fullstack.io",
-						"votes":16
-					},
-					{
-						"title":"Rowland's Homepage",
-						"link":"http://rowlandrose.com",
-						"votes":57
-					}
-				]
-			}
-		}]
-    `);
+    return json;
   }
  
 }
