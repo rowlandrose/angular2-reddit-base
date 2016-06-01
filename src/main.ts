0d2968817be00d281f0b33a70d21fc76cd10c6d5
Rowland Rose <rowlandrose@gmail.com>
2016-05-31 22:53:31 -0400

    adding http providers

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import {HTTP_PROVIDERS} from '@angular/http';
 import { redditApp, environment } from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(redditApp);
+bootstrap(redditApp, [HTTP_PROVIDERS]);
